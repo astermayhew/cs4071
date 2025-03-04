ce47136f675801b20a17e74f02458cda9ccf93aa
aster <whirlwindaster@gmail.com>
2025-03-04 14:06:16 -0500

    i took "inspiration" from petgraphs api

--- hw/3/main.cpp
+++ hw/3/main.cpp
@@ -1,7 +1,10 @@
 #include <algorithm>
-#include <forward_list>
-#include <initializer_list>
+#include <cstddef>
+#include <list>
 #include <iostream>
+#include <memory>
+#include <numeric>
+#include <optional>
 #include <ostream>
 #include <sstream>
 #include <vector>
@@ -14,120 +17,135 @@ template <typename T, typename W> class Graph;
 
 template <typename T, typename W> struct Edge {
   W weight;
-  Vertex<T, W> &from;
-  Vertex<T, W> &to;
+  size_t from;
+  size_t to;
+};
+
+template <typename T, typename W> struct EdgeIndex {
+private:
+  template <typename Tp, typename Wp> friend class Graph;
+  typedef typename std::list<Edge<T, W>>::iterator iterator;
+  iterator edge_iterator;
+
+  EdgeIndex(iterator it) : edge_iterator(it) {}
 };
 
 template <typename T, typename W> struct Vertex {
   Vertex(T value) : data(value) {}
 
   T data;
-  std::forward_list<Edge<T, W>> adjacency_list;
+  std::list<Edge<T, W>> adjacency_list;
 
   bool operator==(const T& rhs) {
     return data == rhs;
   }
 };
 
-template <typename T, typename F> bool remove_one_if(std::forward_list<T> &list, F predicate) {
-  auto leader = list.begin();
-  if (leader == list.end()) {
-    return false;
-  }
+struct VertexIndex {
+private:
+  template <typename Tp, typename Wp> friend class Graph;
+  size_t index;
 
-  if (predicate(*leader)) {
-    list.pop_front();
-    return true;
-  }
-
-  auto follower = leader++;
-
-  while (leader != list.end()) {
-    if (predicate(*leader)) {
-      list.erase_after(follower);
-      return true;
-    }
-    ++leader;
-  }
-
-  return false;
-}
+  VertexIndex(size_t idx) : index(idx) {}
+};
 
 template <typename T, typename W> class Graph {
 private:
   std::vector<Vertex<T, W>> vertices;
 
 public:
-  Graph(std::initializer_list<T> l) {
-    for (auto v : l) {
-      vertices.push_back(v);
-    }
-  }
+  typedef EdgeIndex<T, W> edge_index;
+  typedef VertexIndex vertex_index;
 
-  bool add_vertex(T value) {
-    if (std::find(vertices.begin(), vertices.end(), value) != vertices.end()) {
-      return false;
-    } else {
-      vertices.push_back(value);
-      return true;
-    }
-  }
-
-  void delete_vertex(const T& value) {
-    auto old = vertices.end();
-    for (auto it = vertices.begin(); it != vertices.end(); ++it) {
-      if (*it == value) {
-        old = it;
-      } else {
-        remove_one_if(it->adjacency_list, [value](auto edge) {
-          return edge.to.data == value;
-        });
+  std::optional<vertex_index> find_vertex(const T& value) {
+    for (size_t i = 0; i < vertices.size(); ++i) {
+      if (vertices[i] == value) {
+        return VertexIndex(i);
       }
     }
 
-    vertices.erase(old);
+    return {};
   }
 
-  bool add_edge(const T &from, const T &to, W weight) {
-    auto from_vertex = std::find(vertices.begin(), vertices.end(), from);
-    if (from_vertex == vertices.end()) {
-      return false;
-    }
-
-    auto to_vertex = std::find(vertices.begin(), vertices.end(), to);
-    if (to_vertex == vertices.end()) {
-      return false;
-    }
-
-    if (std::find_if(from_vertex->adjacency_list.begin(),
-                     from_vertex->adjacency_list.end(), [to](auto edge) {
-                       return edge.to == to;
-                     }) != from_vertex->adjacency_list.end()) {
-      return false;
-    }
-
-    from_vertex->adjacency_list.push_front(Edge<T, W>{weight, *from_vertex, *to_vertex});
-    return true;
+  vertex_index add_vertex(T value) {
+    vertices.push_back(value);
+    return vertices.size() - 1;
   }
 
-  bool delete_edge(const T &from, const T &to) {
-    auto from_vertex = std::find(vertices.begin(), vertices.end(), from);
-    if (from_vertex == vertices.end()) {
-      return false;
+  T& vertex_data(vertex_index vertex_index) {
+    return vertices[vertex_index].data;
+  }
+
+  T remove_vertex(vertex_index edge_index) {
+    size_t removal_index = edge_index.index;
+    size_t replacement_index = vertices.size() - 1;
+
+    for (auto &vertex : vertices) {
+      if (std::addressof(vertex) != vertices.data() + removal_index) {
+        auto iterator = vertex.adjacency_list.begin();
+        while (iterator != vertex.adjacency_list.end()) {
+          if (iterator->to == removal_index) {
+            vertex.adjacency_list.erase(iterator++);
+          } else if (iterator->to == replacement_index) {
+            iterator->to = removal_index;
+            ++iterator;
+          } else {
+            ++iterator;
+          }
+        }
+      }
     }
 
-    return remove_one_if(from_vertex->adjacency_list, [to](auto edge) {
+    T return_value = vertices[removal_index].data;
+    if (removal_index != replacement_index) {
+      std::swap(vertices[removal_index], vertices[replacement_index]);
+    }
+    vertices.pop_back();
+    return return_value;
+  }
+
+  std::optional<edge_index> find_edge(const vertex_index from, const vertex_index to) {
+    auto edge_iterator = std::find_if(vertices[from].adjacency_list.begin(), vertices[from].adjacency_list.end(), [to](auto edge) {
       return edge.to == to;
     });
+
+    if (edge_iterator == vertices[from].end()) {
+      return {};
+    } else {
+      return edge_index(edge_iterator);
+    }
+  }
+
+  edge_index add_edge(const vertex_index from, const vertex_index to, W weight) {
+    vertices[from.index].adjacency_list.push_front(Edge<T, W>{weight, from.index, to.index});
+    return EdgeIndex<T, W>(vertices[from.index].adjacency_list.begin());
+  }
+
+  W& edge_weight(edge_index edge_index) {
+    return edge_index.edge_iterator->weight;
+  }
+
+  vertex_index edge_source(edge_index edge_index) {
+    return edge_index.edge_iterator->from;
+  }
+
+  vertex_index edge_target(edge_index edge_index) {
+    return edge_index.edge_iterator->to;
+  }
+
+  W remove_edge(edge_index edge_index) {
+    W return_value = edge_index.edge_iterator->weight;
+    vertices[edge_index.edge_iterator->from].adjacency_list.erase(edge_index.edge_iterator);
+    return return_value;
   }
 
   std::string to_string() {
     std::ostringstream out;
 
-    for (auto vertex : vertices) {
+    for (auto &vertex : vertices) {
       out << vertex.data << ": ";
-      for (auto edge : vertex.adjacency_list) {
-        out << edge.to.data << '(' << edge.weight << "), ";
+      for (auto &edge : vertex.adjacency_list) {
+        out << vertices[edge.to].data << '(' << edge.weight << "), ";
       }
       out << '\n';
     }
@@ -137,25 +155,30 @@ public:
 };
 
 int main(int argc, char **argv) {
-  Graph<int, int> g({0, 1, 2, 3, 4});
-  g.add_edge(1, 2, 1);
+  Graph<int, int> g;
+  std::vector<Graph<int, int>::vertex_index> indices;
+  for (size_t i = 0; i < 5; ++i) {
+    indices.push_back(g.add_vertex(i));
+  }
+
+  auto to_remove = g.add_edge(indices[1], indices[2], 1);
   std::cout << g.to_string() << '\n';
 
-  g.delete_edge(1, 2);
+  g.remove_edge(to_remove);
   std::cout << g.to_string() << '\n';
 
   g.add_vertex(5);
   std::cout << g.to_string() << '\n';
 
-  g.add_edge(0, 3, 2);
+  g.add_edge(indices[0], indices[3], 2);
   std::cout << g.to_string() << '\n';
 
-  g.add_edge(2, 3, 2);
+  g.add_edge(indices[2], indices[3], 2);
   std::cout << g.to_string() << '\n';
 
-  g.add_edge(2, 5, 2);
+  g.add_edge(indices[2], indices[4], 2);
   std::cout << g.to_string() << '\n';
 
-  g.delete_vertex(3);
+  g.remove_vertex(indices[3]);
   std::cout << g.to_string() << '\n';
 }
